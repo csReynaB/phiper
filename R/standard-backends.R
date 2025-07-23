8972cbf66e02b42b9d1e225467a89ce7488d7b2c
Polymerase3 <polimerazaiii@gmail.com>
2025-07-23 04:27:10 -0400

    Working memory backend for standard workflow

---
+++ R/standard-backends.R
@@ -0,0 +1,64 @@
+#' Build a **memory-backed** `phip_data` object (no DuckDB / Arrow) with
+#' standard worflow
+#'
+#' Reads up one long data frame directly into R and returns an in-memory
+#' `phip_data`. If the `data_long_path` is a directory, it reads all files in
+#' the directory, assuming they have the same file structure. Heavy validation
+#' of matrix content is deliberately
+#' **omitted**—that should be performed by the `phip_data` class validator.
+#'
+#' @param cfg       Named list from `resolve_paths()`; must contain one
+#'   `data_long_path` element. Can be a directory or a file.
+#' @param colmap    List of colnames mapped to the reserved `phip_data`
+#'   colnames. Useful when the colnames in the file are named different than the
+#'   colnames of the `phip_data` object.
+#'
+#' @return A `phip_data` object whose `data_long` slot is a tibble in RAM.
+#' @keywords internal
+.standard_read_memory_backend <- function(cfg, colmap) {
+  ## checking if path is dir or file
+  info <- file.info(cfg$data_long_path)
+
+  # ---------- 1.  read the data files -----------------------------------------
+  if (info$isdir ) {
+    # 1) list all files in the directory (parquet, csv)
+    files <- list.files(
+      path       = cfg$data_long_path,
+      pattern    = "\\.(parquet|parq|pq|csv)$",
+      full.names = TRUE
+    )
+
+    # 2) read each file with .auto_read() and row‑bind into one
+    # data.frame/tibble
+    out <- do.call(
+      rbind,
+      lapply(files, .auto_read)
+    )
+
+  } else {
+    out <- .auto_read(cfg$data_long_path)
+  }
+
+  ## clear memory
+  rm(mats)
+  invisible(gc())
+
+  # ---------- 1.  read the data files -----------------------------------------
+  out <- .rename_to_standard(out, colmap)
+
+  # ---------- 4.  finish ------------------------------------------------------
+  tibble::as_tibble(out)
+}
+
+.rename_to_standard <- function(df, colname_map) {
+  # invert: actual names --> standard names
+  actual_to_std <- setNames(names(colname_map), unlist(colname_map))
+
+  # only keep those that actually exist in df
+  actual_to_std <- actual_to_std[names(actual_to_std) %in% colnames(df)]
+
+  # rename
+  names(df)[names(df) %in% names(actual_to_std)] <- actual_to_std[names(df)[names(df) %in% names(actual_to_std)]]
+
+  df
+}
--- R/standard-convert.R
+++ R/standard-convert.R
@@ -57,12 +57,92 @@ phip_convert <- function(
   ## filter the NULLs
   cfg <- Filter(Negate(is.null), cfg)
 
-  print(cfg)
   # ------------------------------------------------------------------
   # 3. mapping the column names
   # ------------------------------------------------------------------
+  colname_map <- list(
+    sample_id    = sample_id %||% "sample_id",
+    peptide_id   = peptide_id %||% "peptide_id",
+    subject_id   = subject_id %||% "subject_id",
+    timepoint    = timepoint  %||% "timepoint",
+    present      = present %||% "present",
+    fold_change  = fold_change %||% "fold_change",
+    counts_input = counts_input %||% "counts_input",
+    counts_hit   = counts_hit %||% "counts_hit"
+  )
 
   # ------------------------------------------------------------------
   # 4. create the phip_data object with different backends
   # ------------------------------------------------------------------
+  if (backend == "memory") {
+    data_long <- .standard_read_memory_backend(cfg, colname_map)
+
+    # returning the phip_data object
+    new_phip_data(
+      data_long = data_long,
+      comparisons =  NULL,
+      peptide_library = cfg$peptide_library,
+      backend = "memory"
+    )
+
+  } else if (backend == "duckdb") {
+    # using the helper (a lot of code is repeated in duckdb and arrow, so i
+    # decided to export it into a separate internale helper to reuse it)
+    con <- .read_duckdb_backend(cfg, meta_list)
+
+    ## duckdb-specific code
+    long <- dplyr::tbl(con, "final_long")
+
+    comps <- if (DBI::dbExistsTable(con, "comparisons")) {
+      dplyr::tbl(con, "comparisons") |> dplyr::collect()
+    }
+
+    # returning the phip_data object
+    new_phip_data(
+      data_long = long,
+      comparisons = comps,
+      peptide_library = cfg$peptide_library,
+      backend = "duckdb",
+      meta = list(con = con)
+    )
+  } else if (backend == "arrow") {
+    ## check dependency
+    rlang::check_installed(c("arrow"), reason = "arrow backend")
+
+    # same as up - use helper to create the data
+    con <- .read_duckdb_backend(cfg, meta_list)
+
+    # arrow-specific code, create tempdir to store the data
+    arrow_dir <- withr::local_tempdir(
+      pattern = sprintf(
+        "phip_arrow_%s_",
+        format(Sys.time(), "%Y%m%d%H%M%OS6")
+      ),
+      .local_envir = parent.frame()
+    )
+
+    # store the data as .parquet (more efficient than plain .csv)
+    DBI::dbExecute(
+      con,
+      sprintf(
+        "COPY final_long TO %s (FORMAT 'parquet', PER_THREAD_OUTPUT TRUE);",
+        DBI::dbQuoteString(con, arrow_dir)
+      )
+    )
+
+    # open as arrow dataset
+    long <- arrow::open_dataset(arrow_dir)
+    comps <- if (DBI::dbExistsTable(con, "comparisons")) {
+      dplyr::tbl(con, "comparisons") |> dplyr::collect()
+    }
+
+    # returning the phip_data object
+    new_phip_data(
+      data_long = long,
+      comparisons = comps,
+      peptide_library = cfg$peptide_library,
+      backend = "arrow",
+      meta = list(parquet_dir = arrow_dir)
+    )
+  }
 }
