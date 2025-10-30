9438edf698b97901ae170841a189b17c5721a3e9
Polymerase3 <polimerazaiii@gmail.com>
2025-10-30 20:51:30 +0100

    Dummy roxygen2 to populate NAMESPACE

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+#' phiper
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @useDynLib phiper, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+#' @importFrom RcppParallel setThreadOptions
+## usethis namespace: end
+NULL
